7d2ae8da8f1d7e12dc4087366a3659e70868baef
geekane <32669380+geekane@users.noreply.github.com>
2025-12-18 13:42:25 +0800

    Update provider-models.js
    加入对于Gemini-3-flash的支持

--- src/provider-models.js
+++ src/provider-models.js
@@ -10,7 +10,8 @@ export const PROVIDER_MODELS = {
         'gemini-2.5-pro',
         'gemini-2.5-pro-preview-06-05',
         'gemini-2.5-flash-preview-09-2025',
-        'gemini-3-pro-preview'
+        'gemini-3-pro-preview',
+        'gemini-3-flash-preview'
     ],
     'gemini-antigravity': [
         'gemini-2.5-computer-use-preview-10-2025',
